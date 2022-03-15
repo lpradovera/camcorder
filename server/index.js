9aa662a9bbf7820c4747c60c13400db9d76841ef
Luca Pradovera <luca.pradovera@gmail.com>
2022-03-15 10:06:02 +0100

    Working Tailwind, start of routing

---
+++ server/index.js
@@ -0,0 +1,69 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const axios = require("axios");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const auth = {
+  username: process.env.SIGNALWIRE_PROJECT_KEY, // Project-ID
+  password: process.env.SIGNALWIRE_TOKEN, // API token
+};
+const apiurl = process.env.SIGNALWIRE_SPACE;// <your username>.signalwire.com/api/video
+
+const permissions = [
+  'room.self.audio_mute',
+  'room.self.audio_unmute',
+  'room.self.video_mute',
+  'room.self.video_unmute',
+  'room.self.deaf',
+  'room.self.undeaf',
+  'room.self.set_input_volume',
+  'room.self.set_output_volume',
+  'room.self.set_input_sensitivity',
+  'room.list_available_layouts',
+  'room.set_layout',
+  'room.member.video_mute',
+  'room.member.audio_mute',
+  'room.member.remove',
+  'room.recording',
+  'room.playback'
+];
+
+app.post("/api/get_token", async (req, res) => {
+  let { user_name, room_name } = req.body;
+
+  try {
+    let token = await axios.post(
+      apiurl + "/api/video/room_tokens",
+      {
+        user_name,
+        room_name: room_name,
+        permissions
+      },
+      { auth }
+    );
+
+    token = token.data?.token;
+    return res.json({ token });
+  } catch (e) {
+    console.log(e);
+    return res.sendStatus(500);
+  }
+});
+
+app.get("/api/test", (req, res) => {
+  console.log('API call')
+  res.json({ message: "Hello from server!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+
