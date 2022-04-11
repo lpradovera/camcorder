bfe46a173ded93c2ec07918c27aba524faf61293
Daniil Zahliadov <portal.sid.grand2@gmail.com>
2022-04-11 18:41:45 +0300

    PhoneMissedCallButton component is divided into subcomponents

--- src/components/InCall/components/PhoneMissedCallButton/PhoneMissedCallButton.jsx
+++ src/components/InCall/components/PhoneMissedCallButton/PhoneMissedCallButton.jsx
@@ -1,35 +1,35 @@
 import React from "react";
 import { PhoneMissedCall } from "../../../Icons/PhoneMissedCall/PhoneMissedCall";
-import { useNavigate } from "react-router-dom";
-import {useDispatch, useSelector } from 'react-redux';
-import { roomLeave } from "../../../../features/roomSlice";
-import { isEmpty } from '../../../../helpers/helpers';
+// import { useNavigate } from "react-router-dom";
+// import { useDispatch, useSelector } from "react-redux";
+// import { roomLeave } from "../../../../features/roomSlice";
+// import { isEmpty } from "../../../../helpers/helpers";
+import { Wrapper } from "./components/Wrapper/Wrapper";
+import { Button } from './components/Button/Button';
 
 export const PhoneMissedCallButton = () => {
-  let navigate = useNavigate();
-  const dispatch = useDispatch();
-  const room = useSelector(state => state.room.room)
- 
-  const handleRoomLeave = () => {
-    if (isEmpty(room)) return
-    dispatch(roomLeave());
 
-    setTimeout(() => {
-      navigate("/", {
-        state: { },
-      });
-    }, 1500);
-  };
+  // let navigate = useNavigate();
+  // const dispatch = useDispatch();
+  // const room = useSelector((state) => state.room.room);
+
+  // const handleRoomLeave = () => {
+  //   if (isEmpty(room)) return;
+  //   dispatch(roomLeave());
+
+  //   setTimeout(() => {
+  //     navigate("/", {
+  //       state: {},
+  //     });
+  //   }, 1500);
+  // };
 
   return (
-    <div className="flex flex-col justify-center px-2 pb-4">
-      <button
-        className="flex dark:bg-red-600 hover:dark:bg-red-500 rounded justify-center pt-4 w-14 h-14"
-        onClick={async () => handleRoomLeave()}
-      >
+    <Wrapper>
+      <Button>
         <PhoneMissedCall />
-      </button>
+      </Button>
       <p className="text-center pt-1 text-slate-300">Leave</p>
-    </div>
+    </Wrapper>
   );
 };
