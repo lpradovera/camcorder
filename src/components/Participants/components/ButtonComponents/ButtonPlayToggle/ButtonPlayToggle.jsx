bf888e831b15cde428f30e618a5f06f9859ea66f
Daniil Zahliadov <portal.sid.grand2@gmail.com>
2022-04-12 10:52:34 +0300

    Broken down into subcomponents ButtonPlayToggle

--- src/components/Participants/components/ButtonComponents/ButtonPlayToggle/ButtonPlayToggle.jsx
+++ src/components/Participants/components/ButtonComponents/ButtonPlayToggle/ButtonPlayToggle.jsx
@@ -1,18 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Play } from "../../../../Icons/Play/Play";
 import { Pause } from "../../../../Icons/Pause/Pause";
 import { Stop } from "../../../../Icons/Stop/Stop";
-import { useDispatch } from "react-redux";
+import { Download } from "../../../../Icons/Download/Download";
+import { useDispatch, useSelector } from "react-redux";
 import {
   play,
   resume,
   pause,
   stop,
+  saveThisRecord,
 } from "../../../../../features/recordingSlice";
-
 
 export const ButtonPlayToggle = ({ id }) => {
   const [waiting, setWaiting] = useState(false);
+  const uri = useSelector((state) => state.recording.uri);
   const dispatch = useDispatch();
 
   const handlePlay = (id) => {
@@ -20,8 +22,8 @@ export const ButtonPlayToggle = ({ id }) => {
       dispatch(resume());
       setWaiting(false);
     } else {
-        dispatch(play(id));
-        setWaiting(true);
+      dispatch(play(id));
+      setWaiting(true);
     }
   };
 
@@ -35,25 +37,32 @@ export const ButtonPlayToggle = ({ id }) => {
     setWaiting(false);
   };
 
+  useEffect(() => {
+    dispatch(saveThisRecord(id));
+  }, [dispatch]);
+
   return (
     <>
       <button
         onClick={() => handlePlay(id)}
-        className="absolute right-12 bottom-0"
+        className="absolute right-20 bottom-0"
       >
         <Play />
       </button>
       <button
         onClick={() => handlePause()}
-        className="absolute right-6 bottom-0"
+        className="absolute right-14 bottom-0"
       >
         <Pause />
       </button>
       <button
         onClick={() => handleStop()}
-        className="absolute right-0 bottom-0"
+        className="absolute right-8 bottom-0"
       >
         <Stop />
+      </button>
+      <button className="absolute right-0 bottom-0">
+        <Download uri={uri} />
       </button>
     </>
   );
