96ea5f9633aa331d26565b12141106fd907b773c
Daniil Zahliadov <portal.sid.grand2@gmail.com>
2022-04-12 11:00:09 +0300

    Broken down into subcomponents ScreenShareButton

--- src/components/InCall/components/ScreenShareButton/ScreenShareButton.jsx
+++ src/components/InCall/components/ScreenShareButton/ScreenShareButton.jsx
@@ -2,26 +2,28 @@ import React from "react";
 import { ScreenShare } from "../../../Icons/ScreenShare/ScreenShare";
 import { useDispatch, useSelector } from "react-redux";
 import { shareScreen } from "../../../../features/layoutSlice";
-import {isEmpty} from '../../../../helpers/helpers';
+import { isEmpty } from "../../../../helpers/helpers";
+import { Wrapper } from "./components/Wrapper/Wrapper";
+import { ButtonName } from "../ButtonName/ButtonName";
 
 export const ScreenShareButton = () => {
   const dispatch = useDispatch();
   const room = useSelector((state) => state.room.room);
 
   const screenShareToggle = () => {
-    if (isEmpty(room)) return
+    if (isEmpty(room)) return;
     dispatch(shareScreen());
   };
 
   return (
-    <div className="flex flex-col justify-center px-2 pb-4">
+    <Wrapper>
       <button
         onClick={() => screenShareToggle()}
         className="flex dark:bg-slate-500 hover:dark:bg-slate-400 rounded justify-center pt-4 w-14 h-14"
       >
         <ScreenShare />
       </button>
-      <p className="text-center pt-1 text-slate-300">Screen</p>
-    </div>
+      <ButtonName name={"Screen"} />
+    </Wrapper>
   );
 };
