3c7db6f7266ccb6833afad47042347a03078341b
Daniil Zahliadov <portal.sid.grand2@gmail.com>
2022-04-12 11:04:37 +0300

    Broken down into subcomponents RecordingButton

--- src/components/InCall/components/GetRecordButton/GetRecordButton.jsx
+++ src/components/InCall/components/GetRecordButton/GetRecordButton.jsx
@@ -1,24 +1,16 @@
 import React from "react";
-import { Save } from "../../../Icons/Save/Save";
+import { ButtonName } from "../ButtonName/ButtonName";
+import { Wrapper } from "./components/Wrapper/Wrapper";
+import { Button } from "./components/Button/Button";
+import { OnRecording } from "./components/OnRecording/OnRecording";
 
-export const GetRecordButton = ({recordingReady}) => {
+export const GetRecordButton = ({ recordingReady }) => {
   return (
-    <div className="flex flex-col justify-center px-2 pb-4">
-      <button
-        className={`flex dark:bg-slate-500 ${
-          recordingReady && "hover:dark:bg-slate-400"
-        } rounded justify-center pt-4 w-14 h-14`}
-      >
-        {recordingReady ? (
-          <Save recordingReady={recordingReady} />
-        ) : (
-          <div className="relative">
-            <Save />
-            <div className="border-r-2 h-7 border-slate-200 rotate-[-45deg] absolute top-[-2px] left-[10px]"></div>
-          </div>
-        )}
-      </button>
-      <p className="text-center pt-1 text-slate-300">Save</p>
-    </div>
+    <Wrapper>
+      <Button recordingReady={recordingReady}>
+        <OnRecording recordingReady={recordingReady} />
+      </Button>
+      <ButtonName name={"Save"} />
+    </Wrapper>
   );
 };
