6799e513e6948171e916187c01cfe41bf47a26ed
Daniil Zahliadov <portal.sid.grand2@gmail.com>
2022-04-12 11:08:20 +0300

    modified PhoneMissedCallButton

--- src/components/InCall/components/PhoneMissedCallButton/PhoneMissedCallButton.jsx
+++ src/components/InCall/components/PhoneMissedCallButton/PhoneMissedCallButton.jsx
@@ -1,35 +1,16 @@
 import React from "react";
 import { PhoneMissedCall } from "../../../Icons/PhoneMissedCall/PhoneMissedCall";
-// import { useNavigate } from "react-router-dom";
-// import { useDispatch, useSelector } from "react-redux";
-// import { roomLeave } from "../../../../features/roomSlice";
-// import { isEmpty } from "../../../../helpers/helpers";
 import { Wrapper } from "./components/Wrapper/Wrapper";
-import { Button } from './components/Button/Button';
+import { Button } from "./components/Button/Button";
+import { ButtonName } from "../ButtonName/ButtonName";
 
 export const PhoneMissedCallButton = () => {
-
-  // let navigate = useNavigate();
-  // const dispatch = useDispatch();
-  // const room = useSelector((state) => state.room.room);
-
-  // const handleRoomLeave = () => {
-  //   if (isEmpty(room)) return;
-  //   dispatch(roomLeave());
-
-  //   setTimeout(() => {
-  //     navigate("/", {
-  //       state: {},
-  //     });
-  //   }, 1500);
-  // };
-
   return (
     <Wrapper>
       <Button>
         <PhoneMissedCall />
       </Button>
-      <p className="text-center pt-1 text-slate-300">Leave</p>
+      <ButtonName name={"Leave"} />
     </Wrapper>
   );
 };
