718e7c6628680a48695659acc5d633f325f048e6
Luca Pradovera <luca.pradovera@gmail.com>
2022-04-15 10:56:27 +0200

    Fix path reference

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
 const { permissionsCheck } = require("./helpers");
+const path = require('path');
 
 const PORT = process.env.PORT || 9001;
 const app = express();
